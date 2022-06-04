c091445091b6a4a3fce6c0d875dda58325636e73
DimitarStoimenov <stoimenovdimitur@yahoo.com>
2022-06-05 00:07:36 +0200

    C#

---
+++ Add VAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal> input = x => x += x * 0.2m;
+            decimal[] upperstr = Console.ReadLine().Split(", ").Select(decimal.Parse).Select(input).ToArray();
+            foreach (decimal item in upperstr)
+            {
+                Console.WriteLine($"{item:f2}");
+            };
+
+
+        }
+    }
+}
---
+++ Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> word = x => x.Length > 0 && char.IsUpper(x[0]);
+
+            string[] input = Console.ReadLine().Split().Where(word).ToArray();
+            Console.WriteLine(string.Join("\r\n", input));
+        }
+    }
+}
